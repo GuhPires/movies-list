92e29c2c548827145443d599db429488eee5c5ac
GuhPires <gustavo.goncalvespires@gmail.com>
2022-06-10 18:40:06 -0300

    add enums

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import TBDMClient from './utils/TMDBClient';
+import { LIST_TYPES, LIST_CONTENT_TYPES } from './enums';
 
-const client = new TBDMClient();
+const MOCKED_LISTS = [
+  {
+    _id: '01',
+    type: LIST_TYPES.FAVORITE,
+    contentType: LIST_CONTENT_TYPES.MOVIES,
+    content: []
+  },
+  {
+    _id: '02',
+    type: LIST_TYPES.WATCHED,
+    contentType: LIST_CONTENT_TYPES.MOVIES,
+    content: []
+  },
+  {
+    _id: '03',
+    type: LIST_TYPES.WATCH_LIST,
+    contentType: LIST_CONTENT_TYPES.MOVIES,
+    content: []
+  },
+]
+
 
 function App() {
-  return <h1>Hello!</h1>;
+  return <h1>Movies List!</h1>;
 }
 
 export default App;
---
+++ src/enums/list.js
@@ -0,0 +1,10 @@
+export const LIST_TYPES = {
+  FAVORITE: 'favorite',
+  WATCHED: 'watched',
+  WATCH_LIST: 'watch_list',
+};
+
+export const LIST_CONTENT_TYPES = {
+  MOVIES: 'movies',
+  SHOWS: 'shows',
+};
